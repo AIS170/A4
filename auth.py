8d90f633d3cf313f4597f66a5ad1896598e3c0ca
Aakash Dilip <z5477304@ad.unsw.edu.au>
2024-03-02 11:17:37 +1100

    Added stub code for login function

--- auth.py
+++ auth.py
@@ -3,3 +3,7 @@
 # Registers a new user and redirects them to their mailbox
 def signup(firstName: str, lastName: str, email: str, password: str, confirmPassword: str) -> str:
     return 'u1'
+
+# Login page for registered users. Redirects the user to their mailbox
+def login(email: str, password: str) -> str:
+    return 'u1'
