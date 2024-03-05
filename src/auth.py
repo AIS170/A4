a3adc1c4d6058d485a8062e25333834daad74bda
z5425705 <z5425705@vx16.orchestra.cse.unsw.EDU.AU>
2024-03-06 01:07:23 +1100

    made a basic server, and linked backend and basic frontend

--- src/auth.py
+++ src/auth.py
@@ -1,9 +1,20 @@
 # Stub code for authorisation functions
+from flask import Blueprint, render_template, request, redirect, url_for
 
+authenticateUser = Blueprint('authenticate_user', __name__)
+
+@authenticateUser.route('/a4/signup/', methods=['GET', 'POST'])
 # Registers a new user and redirects them to their mailbox
-def signup(firstName: str, lastName: str, email: str, password: str, confirmPassword: str) -> str:
-    return 'u1'
+def signup():
+    if request.method == 'POST':
+        # firstName, lastName, email, password, confirmPassword
+        return 'u1'
+    return render_template('register.html')
 
 # Login page for registered users. Redirects the user to their mailbox
-def login(email: str, password: str) -> str:
-    return 'u1'
+@authenticateUser.route('/a4/login/', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        # email, password
+        return 'u1'
+    return render_template('login.html')
