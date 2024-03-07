cca6331ee2a5eca651595afb2d44e8400811e8ef
Aakash Dilip <z5477304@ad.unsw.edu.au>
2024-03-07 20:37:14 +1100

    Added basic functionality for signup function

--- src/auth.py
+++ src/auth.py
@@ -1,5 +1,7 @@
 # Stub code for authorisation functions
 from flask import Blueprint, render_template, request, redirect, url_for
+import uuid
+import re
 
 authenticateUser = Blueprint('authenticate_user', __name__)
 
@@ -7,9 +9,40 @@ authenticateUser = Blueprint('authenticate_user', __name__)
 # Registers a new user and redirects them to their mailbox
 def signup():
     if request.method == 'POST':
-        # firstName, lastName, email, password, confirmPassword
-        return 'u1'
+        first_name = request.form['firstName']
+        last_name = request.form['lastName']
+        email = request.form['email']
+        password = request.form['password']
+        confirm_password = request.form['confirmPassword']
+        if password != confirm_password:
+            return 'Passwords do not match', 400
+        elif len(first_name) > 15:
+            return 'First name cannot exceed 15 characters', 400
+        elif len(first_name) == 0:
+            return 'First name cannot be empty', 400
+        elif len(last_name) > 15:
+            return 'Last name cannot exceed 15 characters', 400
+        elif len(last_name) == 0:
+            return 'Last name cannot be empty', 400
+        elif not validEmail(email):
+            return 'Email must be of a valid format', 400
+        elif len(password) < 8:
+            return 'Password should be atleast 8 characters', 400
+        elif len(password) > 15:
+            return 'Password shouldn\'t exceed 15 characters', 400
+        elif not (any(char.isalpha() for char in password) and any(char.isdigit() for char in password)):
+            return 'Password should contain atleast one letter and one number', 400
+        else:
+            return uuid.uuid4()
     return render_template('register.html')
+
+def validEmail(email: str) -> bool:
+    regexPattern = r'^[a-zA-Z0-9._]+@[a-zA-Z0-9]+\.[a-zA-Z]{2,}$'
+    if re.match(regexPattern, email):
+        return True
+    else:
+        return False
+
 
 # Login page for registered users. Redirects the user to their mailbox
 @authenticateUser.route('/a4/login/', methods=['GET', 'POST'])
