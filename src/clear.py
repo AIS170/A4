0240a6df514e44b084ee8ba85d7ac9daeb44a4db
Aakash Dilip <z5477304@ad.unsw.edu.au>
2024-03-10 17:53:46 +1100

    Turned clear function into its own route

--- src/clear.py
+++ src/clear.py
@@ -1,8 +1,13 @@
 from .database import db
 from .models import User, Token, Invoice, CommunicationReport 
+from flask import Blueprint
+
+clear_ = Blueprint('clear_route', __name__)
+
+@clear_.route('', methods=['DELETE'])
 def clear():
     db.session.query(User).delete()
     db.session.query(Token).delete()
     db.session.query(Invoice).delete()
     db.session.query(CommunicationReport).delete()
-    db.session.commit
+    db.session.commit()
