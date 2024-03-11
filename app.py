075a683dfd424e60bccbc4da1a4378720799e76a
rakshil17 <161939734+rakshil17@users.noreply.github.com>
2024-03-11 12:51:57 +1100

    Changing debug to false on app.py

--- app.py
+++ app.py
@@ -30,4 +30,4 @@ app.register_blueprint(clear_, url_prefix='/clear')
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-    app.run(debug=True)
+    app.run(debug=False)
