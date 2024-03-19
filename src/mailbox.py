76a6010ed75d80540ab7bbe95efa4d5c832a61bd
Aakash Dilip <z5477304@ad.unsw.edu.au>
2024-03-19 22:49:27 +1100

    Edited certain error messages

--- src/mailbox.py
+++ src/mailbox.py
@@ -42,23 +42,21 @@ def sending():
     if request.method == 'POST':
         user_id_a = session.get('user_id')
         if user_id_a == None:
-            return jsonify({'error': 'Invalid user ID'}), 400
+            return jsonify({'error': 'Invalid userId'}), 400
         recipient_address = request.form.get('recipient_address')
         invoice_subject = request.form.get('invoice_subject')
-        # invoice_body = request.form.get('invoice_body')
-            
+        # invoice_body = request.form.get('invoice_body')   
         recipient = User.query.filter_by(email=recipient_address).first()
-        if recipient is None:
+        if recipient == None:
             return jsonify({'error': 'Recipient does not exist'}), 404
-        if invoice_subject == None:
-            return jsonify({'error': 'Subject cannot be null'}), 400
+        if invoice_subject == '':
+            return jsonify({'error': 'Subject cannot be empty'}), 400
         if len(invoice_subject) > 50:
             return jsonify({'error': 'Subject cannot be over 50 characters long'}), 400
         # if invoice_body == None:
         #     return jsonify({'error': 'Body cannot be null'}), 400
         # elif len(invoice_body) > 1000:
         #     return jsonify({'error': 'Body cannot be over 1000 characters long'}), 400
-        
         xml_file = request.files.get('invoice_file')
         if xml_file and allowed_file(xml_file.filename):
             xml_content = xml_file.read()
@@ -68,7 +66,7 @@ def sending():
             new_mail = Invoice(id=os.urandom(24).hex(), subject=invoice_subject, body=invoice_json, date_sent=datetime.now(), user_id=user_id_a, is_incoming=False, sent_to_user_id=recipient.id)
             db.session.add(new_mail)
             db.session.commit()
-
+            
             report_details = "Invoice Sent Details:\nSubject: {}\nRecipient: \nSender: {}\nDate Sent: {}".format(invoice_subject, recipient_address, user_id_a, datetime.now())
             new_comm_report = CommunicationReport(id=os.urandom(24).hex(), invoice_id=new_mail.id, details=report_details, date_reported=datetime.now())
             db.session.add(new_comm_report)
