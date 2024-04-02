e87e422e03c3021c3013f0a0d3a4e04d75e7eb1b
Aakash Dilip <z5477304@ad.unsw.edu.au>
2024-04-02 12:21:14 +1100

    Created test file for frontend to check correct building of frontend

---
+++ tests/test_frontend.py
@@ -0,0 +1,24 @@
+import pytest
+from app import app
+
+@pytest.fixture
+def client():
+    app.testing = True
+    with app.test_client() as client:
+        yield client
+
+def test_frontend_signup_html(client):
+    response = client.get('/auth/signup', follow_redirects=True)
+    assert response.status_code == 200
+
+def test_frontend_login_html(client):
+    response = client.get('/auth/login', follow_redirects=True)
+    assert response.status_code == 200
+
+def test_frontend_login_html(client):
+    response = client.get('/mailbox/sending', follow_redirects=True)
+    assert response.status_code == 200
+
+def test_frontend_sending_html(client):
+    response = client.get('/mailbox/sending', follow_redirects=True)
+    assert response.status_code == 200
