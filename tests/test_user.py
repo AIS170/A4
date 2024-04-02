18105d48b6a237f989c1daedd045d452c459fb6e
Aakash Dilip <z5477304@ad.unsw.edu.au>
2024-04-02 12:55:06 +1100

    Added test cases for email change route

--- tests/test_user.py
+++ tests/test_user.py
@@ -72,3 +72,71 @@ def test_user_details_invalid_user_id():
     assert response.status_code == 400
     message = json.loads(response.data)
     assert message['error'] == 'Invalid userId'
+
+# ================================================
+# ==== Test Cases for the Change Email route =====
+# ================================================
+    
+# Test email changed successfully
+def test_email_change_success():
+    client = app.test_client()
+    client.delete('/clear')
+    client.post('/auth/signup', data=valid_registration_data1, follow_redirects=True)
+    client.post('/auth/login', data=valid_login_data1, follow_redirects=True)
+
+    response = client.put('/user/email', data={'new_email': "newemail@newemail.com"}, follow_redirects=True)
+    assert response.status_code == 200
+
+    with app.app_context():
+        user = User.query.filter_by(email="newemail@newemail.com").first()
+        assert user is not None
+    
+# Test email change not logged in
+def test_email_change_invalid_user_id():
+    client = app.test_client()
+    client.delete('/clear')
+    client.post('/auth/signup', data=valid_registration_data1, follow_redirects=True)
+
+    response = client.put('/user/email', data={'new_email': "newemail@newemail.com"}, follow_redirects=True)
+    assert response.status_code == 400
+    message = json.loads(response.data)
+    assert message['error'] == 'Invalid userId'
+
+# Test email change invalid email format
+def test_email_change_invalid_email():
+    client = app.test_client()
+    client.delete('/clear')
+    client.post('/auth/signup', data=valid_registration_data1, follow_redirects=True)
+    client.post('/auth/login', data=valid_login_data1, follow_redirects=True)
+
+    response = client.put('/user/email', data={'new_email': "newemail.com"}, follow_redirects=True)
+    assert response.status_code == 400
+    message = json.loads(response.data)
+    assert message['error'] == 'Email must be of a valid format'
+
+# Test email change new email same as current email
+def test_email_change_new_email_same_as_current_email():
+    client = app.test_client()
+    client.delete('/clear')
+    client.post('/auth/signup', data=valid_registration_data1, follow_redirects=True)
+    client.post('/auth/login', data=valid_login_data1, follow_redirects=True)
+
+    response = client.put('/user/email', data={'new_email': "user@example.com"}, follow_redirects=True)
+    assert response.status_code == 400
+    message = json.loads(response.data)
+    assert message['error'] == 'New email is the same as current email'
+
+# Test email change new email already in use
+def test_email_change_email_in_use():
+    client = app.test_client()
+    client.delete('/clear')
+    client.post('/auth/signup', data=valid_registration_data1, follow_redirects=True)
+    client.post('/auth/login', data=valid_login_data1, follow_redirects=True)
+    client.get('/auth/logout', follow_redirects=True)
+    client.post('/auth/signup', data=valid_registration_data2, follow_redirects=True)
+    client.post('/auth/login', data=valid_login_data2, follow_redirects=True)
+
+    response = client.put('/user/email', data={'new_email': "user@example.com"}, follow_redirects=True)
+    assert response.status_code == 400
+    message = json.loads(response.data)
+    assert message['error'] == 'Email is already in use'
