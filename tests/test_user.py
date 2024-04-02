19da6be4f9dc816dfe421db0939e575b98641b0c
Aakash Dilip <z5477304@ad.unsw.edu.au>
2024-04-02 13:11:25 +1100

    Added test cases for change password route

--- tests/test_user.py
+++ tests/test_user.py
@@ -5,7 +5,7 @@ import json
 from io import BytesIO
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from app import app
-from src.models import User, Token, Invoice, CommunicationReport
+from src.models import User
 
 BASE_URL = "http://localhost:5000"
 
@@ -140,3 +140,80 @@ def test_email_change_email_in_use():
     assert response.status_code == 400
     message = json.loads(response.data)
     assert message['error'] == 'Email is already in use'
+
+# ================================================
+# === Test Cases for the Change Password route ===
+# ================================================
+    
+# Test password changed successfully
+def test_password_change_success():
+    client = app.test_client()
+    client.delete('/clear')
+    client.post('/auth/signup', data=valid_registration_data1, follow_redirects=True)
+    client.post('/auth/login', data=valid_login_data1, follow_redirects=True)
+
+    response = client.put('/user/password', data={'new_password': "str0ngPass"}, follow_redirects=True)
+    assert response.status_code == 200
+
+    with app.app_context():
+        user = User.query.filter_by(password="str0ngPass").first()
+        assert user is not None
+    
+# Test password change not logged in
+def test_password_change_invalid_user_id():
+    client = app.test_client()
+    client.delete('/clear')
+    client.post('/auth/signup', data=valid_registration_data1, follow_redirects=True)
+
+    response = client.put('/user/password', data={'new_password': "str0ngPass"}, follow_redirects=True)
+    assert response.status_code == 400
+    message = json.loads(response.data)
+    assert message['error'] == 'Invalid userId'
+
+# Test password change password too short
+def test_password_change_password_too_short():
+    client = app.test_client()
+    client.delete('/clear')
+    client.post('/auth/signup', data=valid_registration_data1, follow_redirects=True)
+    client.post('/auth/login', data=valid_login_data1, follow_redirects=True)
+
+    response = client.put('/user/password', data={'new_password': "str0ng"}, follow_redirects=True)
+    assert response.status_code == 400
+    message = json.loads(response.data)
+    assert message['error'] == 'Password should be atleast 8 characters'
+
+# Test password change password too long
+def test_password_change_password_too_long():
+    client = app.test_client()
+    client.delete('/clear')
+    client.post('/auth/signup', data=valid_registration_data1, follow_redirects=True)
+    client.post('/auth/login', data=valid_login_data1, follow_redirects=True)
+
+    response = client.put('/user/password', data={'new_password': "str0ngPassworddd"}, follow_redirects=True)
+    assert response.status_code == 400
+    message = json.loads(response.data)
+    assert message['error'] == 'Password shouldn\'t exceed 15 characters'
+
+# Test password change password doesn't contain atleast one letter
+def test_password_change_password_no_letters():
+    client = app.test_client()
+    client.delete('/clear')
+    client.post('/auth/signup', data=valid_registration_data1, follow_redirects=True)
+    client.post('/auth/login', data=valid_login_data1, follow_redirects=True)
+
+    response = client.put('/user/password', data={'new_password': "000000000"}, follow_redirects=True)
+    assert response.status_code == 400
+    message = json.loads(response.data)
+    assert message['error'] == 'Password should contain atleast one letter and one number'
+
+# Test password change password doesn't contain atleast one number
+def test_password_change_password_no_numbers():
+    client = app.test_client()
+    client.delete('/clear')
+    client.post('/auth/signup', data=valid_registration_data1, follow_redirects=True)
+    client.post('/auth/login', data=valid_login_data1, follow_redirects=True)
+
+    response = client.put('/user/password', data={'new_password': "aaaaaaaaa"}, follow_redirects=True)
+    assert response.status_code == 400
+    message = json.loads(response.data)
+    assert message['error'] == 'Password should contain atleast one letter and one number'
